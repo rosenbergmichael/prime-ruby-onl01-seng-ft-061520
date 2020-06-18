bcbcabba939023503c4db52a2a806a9841f7274d
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-18 22:21:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,17 @@ while
 end 
 
 
-#def is_prime(num)
-#  n = 2
-#  while n < num
-#    return false if num % n == 0
-#    n += 1
-#  end
-#  true
-#end
+def my_all?(collection)
+ i = 0
+ block_return_values = []
+ while i < collection.length
+   block_return_values << yield(collection[i])
+   i = i + 1
+ end
+ 
+ if block_return_values.include?(false)
+   false
+ else
+   true
+ end
+end
