fec1b36f275929c4b48c8822b8210850dca52bbb
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 00:04:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+def prime?(num)
+  if num >= 2 
+    i = 2 
+    while i <= num-1
+      if num%i == 0 
+      return false 
+      end
+      i += 1
+    end 
+  else 
+    return false 
+  end 
+true 
+end 
+
+#def is_prime(num)
+#  (2..(num - 1)).each do |n|
+#    return false if num % n == 0
+#  end
+#  true
+#end
+
